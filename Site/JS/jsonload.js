52108826525616ba58f62a71f15b8fd31c776cbe
Sebastian Ellefsen <sebastae@stud.ntnu.no>
2017-10-03 23:59:25 +0200

    New attempt, kind of working but not really

--- Site/JS/jsonload.js
+++ Site/JS/jsonload.js
@@ -1,19 +1,39 @@
 "use strict";
-var XMLHttpRequest = require("xhr2")
 
+let jfile = fetch("Articles/test.json"); // Async, uses Promise, same problem as before
+console.log(jfile);
 
-function loadJSONFile(file){
-    let rawFile = new XMLHttpRequest();
+function jsonLoad(file){
+    return new Promise(function(resolve, reject){
+        let request = new XMLHttpRequest();
 
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function(){
-        console.log("Readystate: " + rawFile.readyState);
-        if(rawFile.readyState === 4){
-            if(rawFile.status === 200 || rawFile.status === 0){
-                console.log(rawFile.responseText);
-                return JSON.parse(rawFile.responseText);
+        request.open("GET", file);
+        request.onreadystatechange = function(){
+            if(request.readyState === 4){
+                if(request.status === 200 || request.status === 0){
+                    resolve(request.responseText);
+                }
             }
-            
         }
-    }
+        request.onerror = function(){
+            reject(Error("An error occured while trying to load a JSON file"));
+        }
+        request.send();
+    });
 }
+
+function jsonParse(file){
+    return jsonLoad(file).then(function(response){
+        return JSON.parse(response);
+    }, function(response){
+        console.error(response);
+    });
+}
+
+let j = jsonParse("./Articles/test.json");
+console.log(j);
+
+// Ideas:
+// Load JSON into array when fetched, call function to render when article is loaded
+// Only render those who haven't been rendered yet
+// (check DOM for ID?)
