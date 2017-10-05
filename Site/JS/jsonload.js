501ead408e03538aa7477fd4396ab0a8a59c1d83
Sebastian Ellefsen <sebastae@stud.ntnu.no>
2017-10-05 12:14:48 +0200

    New test file, JSON loading now works

--- Site/JS/jsonload.js
+++ Site/JS/jsonload.js
@@ -1,39 +1,22 @@
 "use strict";
 
-let jfile = fetch("Articles/test.json"); // Async, uses Promise, same problem as before
-console.log(jfile);
+let loaded_JSON_files = [];
 
-function jsonLoad(file){
-    return new Promise(function(resolve, reject){
-        let request = new XMLHttpRequest();
+function JSONLoader(file, rendermethod){
+    let request = new XMLHttpRequest;
 
-        request.open("GET", file);
-        request.onreadystatechange = function(){
-            if(request.readyState === 4){
-                if(request.status === 200 || request.status === 0){
-                    resolve(request.responseText);
-                }
+    request.open("GET", file);
+    request.onreadystatechange = function(){
+        if(request.readyState === 4){
+            if(request.status === 200 || request.status === 0){
+                let parsedJSON = JSON.parse(request.responseText);
+                loaded_JSON_files.push(parsedJSON);
+                rendermethod(parsedJSON);
             }
         }
-        request.onerror = function(){
-            reject(Error("An error occured while trying to load a JSON file"));
-        }
-        request.send();
-    });
+    }
+    request.send();
 }
 
-function jsonParse(file){
-    return jsonLoad(file).then(function(response){
-        return JSON.parse(response);
-    }, function(response){
-        console.error(response);
-    });
-}
-
-let j = jsonParse("./Articles/test.json");
-console.log(j);
-
-// Ideas:
-// Load JSON into array when fetched, call function to render when article is loaded
-// Only render those who haven't been rendered yet
-// (check DOM for ID?)
+JSONLoader("/Articles/test.json", out => console.log(out.head.text));
+JSONLoader("/Articles/test_2.json", out => console.log(out.head.text));
